7d7c7383291b1198dd0c73457d5d84fc6f4b2921
OhBonsai <letbonsaibe@gmail.com>
2018-12-21 00:31:17 +0800

    update

--- repl/repl.go
+++ repl/repl.go
@@ -5,25 +5,35 @@ import (
 	"bufio"
 	"fmt"
 	"github.com/OhBonsai/yolang/lexer"
-	"github.com/OhBonsai/yolang/token"
+	"github.com/OhBonsai/yolang/parser"
 )
 
 const PROMPT = ">>"
 
-func Start(in io.Reader, out io.Writer) {
-	scanner := bufio.NewScanner(in)
+
+func Start(in io.Reader, out io.Writer) { scanner := bufio.NewScanner(in)
 	for {
 		fmt.Printf(PROMPT)
 		scanned := scanner.Scan()
+
 		if !scanned {
 			return
 		}
-
 		line := scanner.Text()
 		l := lexer.New(line)
-
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Printf("%+v\n", tok)
+		p := parser.New(l)
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
 		}
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
